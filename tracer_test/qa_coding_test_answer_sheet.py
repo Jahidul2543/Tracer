d31894bc8533acbc05957897914b6846aaffe4dd
Md Jahidul Islam <jahidul@Mds-MBP-2.fios-router.home>
2019-07-15 00:10:38 -0400

    added try catch block in all functions

--- tracer_test/qa_coding_test_answer_sheet.py
+++ tracer_test/qa_coding_test_answer_sheet.py
@@ -5,7 +5,7 @@ Implemented count_odds_and_evens() function in tracer_app.CountOddAndEven.py to 
 whether constructor can invoke the the function while using an instance created by the
  constructor provided in this  class
 """
-import tracer_app.CountOddAndEven
+# import tracer_app.CountOddAndEven
 
 
 class CountOddsAndEvensTests(object):
@@ -44,21 +44,40 @@ class CountOddsAndEvensTests(object):
         # tests that correct output is given when input is empty list of integers
         test_input = []
         expected = {'ODDS': 0, 'EVENS': 0}
-        actual = self.count_odds_and_evens(test_input)
+        actual = None
+        try:
+            actual = self.count_odds_and_evens(test_input)
+        except Exception as detail:
+            print(detail)
+        else:
+            print("No Exception")
+
         return expected == actual
 
     def test_only_even_list(self):
         # tests that correct output is given when input is only even list of integers
         test_input = [2, 4, 6]
         expected = {'ODDS': 0, 'EVENS': 3}
-        actual = self.count_odds_and_evens(test_input)
+        actual = None
+        try:
+            actual = self.count_odds_and_evens(test_input)
+        except Exception as detail:
+            print(detail)
+        else:
+            print("No Exception")
         return expected == actual
 
     def test_only_odd_list(self):
         # tests that correct output is given when input is only odd list of integers
         test_input = [1, 3, 5]
         expected = {'ODDS': 3, 'EVENS': 0}
-        actual = self.count_odds_and_evens(test_input)
+        actual = None
+        try:
+            actual = self.count_odds_and_evens(test_input)
+        except Exception as detail:
+            print(detail)
+        else:
+            print("No Exception")
         return expected == actual
 
     def test_with_non_numeric_value_in_list(self):
@@ -81,11 +100,11 @@ class CountOddsAndEvensTests(object):
 
 
 # Using below instance of this class can be used to text the function written in tracer_test.CountOddAndEven.py
-obj = CountOddsAndEvensTests(tracer_app.CountOddAndEven.count_odds_and_evens)
+# obj = CountOddsAndEvensTests(tracer_app.CountOddAndEven.count_odds_and_evens)
 # now lets call all the test functions and see the result
-print('test_small_list: ' + obj.test_small_list().__str__())
-print("test_large_list: " + obj.test_large_list().__str__())
-print("test_empty_list: " + obj.test_empty_list().__str__())
-print("test_only_even_list: " + obj.test_only_even_list().__str__())
-print("test_only_odd_list: " + obj.test_only_odd_list().__str__())
-print("test_with_non_numeric_value_in_list: " + obj.test_with_non_numeric_value_in_list().__str__())
+# print('test_small_list: ' + obj.test_small_list().__str__())
+# print("test_large_list: " + obj.test_large_list().__str__())
+# print("test_empty_list: " + obj.test_empty_list().__str__())
+# print("test_only_even_list: " + obj.test_only_even_list().__str__())
+# print("test_only_odd_list: " + obj.test_only_odd_list().__str__())
+# print("test_with_non_numeric_value_in_list: " + obj.test_with_non_numeric_value_in_list().__str__())
